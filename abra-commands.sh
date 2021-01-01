c28bb98e232610e9f141fe93547f40108b12c06c
3wc <3wc.git@doesthisthing.work>
2021-01-01 18:58:55 +0200

    App & db backup commands, see coop-cloud/abra#70

--- abra-commands.sh
+++ abra-commands.sh
@@ -9,3 +9,32 @@ sub_wp() {
 	# shellcheck disable=SC2154,SC2086
 	docker run -it --volumes-from "$CONTAINER" --network "container:$CONTAINER" wordpress:cli wp ${abra__args_[*]}
 }
+
+abra_backup_app() {
+  # shellcheck disable=SC2034
+  {
+	abra__src_="app:/var/www/html/wp-content"
+	abra__dst_="-"
+  }
+
+  # shellcheck disable=SC2154
+  FILENAME="$ABRA_DIR/backups/${abra__app_}_wp-content_$(date +%F).tar.gz"
+
+  sub_app_cp | gzip > "$FILENAME"
+}
+
+abra_backup_db() {
+  # shellcheck disable=SC2034
+  abra__service_="db"
+
+  DB_ROOT_PASSWORD=$(sub_app_run cat /run/secrets/db_root_password)
+
+  # shellcheck disable=SC2154
+  FILENAME="$ABRA_DIR/backups/${abra__app_}_$(date +%F).sql.gz"
+
+  sub_app_run mysqldump -u root -p"$DB_ROOT_PASSWORD" wordpress | gzip > "$FILENAME"
+}
+
+abra_backup() {
+  abra_backup_app && abra_backup_db
+}
